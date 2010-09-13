6d221f6d4571b75e56e748ae74822989d6b72bca
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2010-09-13 14:55:23 +0000

    improve comment

--- src/rsc/misc/Registry.h
+++ src/rsc/misc/Registry.h
@@ -176,6 +176,8 @@ private:
  * constructor keyword in static libraries if the *.o file is really used by the
  * binary. Thus it is advisable not to use static libraries for this purpose.
  *
+ * Class names for this macro must be given without namespaces.
+ *
  * @param registryClassName type of the Registry to register in
  * @param registreeClassName concrete registree to register. Must have a default
  *                           constructor.

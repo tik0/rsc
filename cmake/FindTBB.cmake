d1531324be5aae08fbe700b35ec67c330094004d
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2010-11-11 16:57:16 +0000

    fix finding TBB with version information

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -4,9 +4,10 @@
 # TBB_INCLUDE_DIRS - include directories needed to use the C++ driver
 # TBB_LIBRARIES - Libraries to link agains fpr the driver
 # TBB_LIBRARY_DIRS - Directories containing the libraries (win)
+# TBB_VERSION - Version of the TBB library that was found
 #
 # Possible hints:
-# TBB_ROOT - root directory of the mongodb installation
+# TBB_ROOT - root directory of the TBB installation
 # TBB_DEBUG - if true, use the debug libraries of tbb instead of stripped ones
 # TBB_BITS - value of 32 or 64 indicating the os type, default 32
 # TBB_LIB_PATH - path containing the libraries to use
@@ -28,7 +29,8 @@
 INCLUDE(FindPackageHandleStandardArgs)
 
 FIND_PATH(TBB_INCLUDE_DIRS
-          PATHS ${TBB_ROOT}/include
+          HINTS ${TBB_ROOT}/include
+                ${CMAKE_INSTALL_PREFIX}/include
           NAMES tbb.h
           PATH_SUFFIXES tbb)
 
@@ -55,6 +57,7 @@ IF(VMATCH)
 ENDIF()
 
 # decide if 32 or 64 bits
+# TODO jwienke: do this automatically by using one of cmake's size check methods
 IF(NOT TBB_BITS)
     MESSAGE(STATUS "Assuming 32 bit system")
     SET(TBB_BITS "32")
@@ -64,14 +67,20 @@ SET(WIN_LIB_PATH "${TBB_ROOT}/lib/ia${TBB_BITS}/vc${VC_VERSION}")
 SET(TBB_LIBRARY_DIRS "${TBB_ROOT}/bin/ia${TBB_BITS}/vc${VC_VERSION}")
 
 FIND_LIBRARY(TBB_LIBRARY
-             PATHS ${TBB_ROOT}/lib
+             HINTS ${TBB_ROOT}/lib
+                   ${TBB_ROOT}
                    ${TBB_LIB_PATH}
                    ${WIN_LIB_PATH}
+                   ${CMAKE_INSTALL_PREFIX}
+                   ${CMAKE_INSTALL_PREFIX}/lib
              NAMES ${TBB_LIB_NAME})
 FIND_LIBRARY(TBBMALLOC_LIBRARY
-             PATHS ${TBB_ROOT}/lib
+             HINTS ${TBB_ROOT}/lib
+                   ${TBB_ROOT}
                    ${TBB_LIB_PATH}
                    ${WIN_LIB_PATH}
+                   ${CMAKE_INSTALL_PREFIX}
+                   ${CMAKE_INSTALL_PREFIX}/lib
              NAMES ${TBBMALLOC_LIB_NAME})
              
 IF(TBB_LIBRARY AND TBBMALLOC_LIBRARY)
@@ -79,14 +88,39 @@ IF(TBB_LIBRARY AND TBBMALLOC_LIBRARY)
 ENDIF()
              
 # post-process inlude path
-MESSAGE(STATUS "raw TBB include dirs: ${TBB_INCLUDE_DIRS}")
 IF(TBB_INCLUDE_DIRS)
     STRING(REGEX REPLACE tbb/?$$ "" TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIRS})
 ENDIF()
-MESSAGE(STATUS "final TBB include dirs: ${TBB_INCLUDE_DIRS}")
 SET(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIRS} CACHE PATH "TBB include dirs" FORCE)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(TBB DEFAULT_MSG TBB_INCLUDE_DIRS TBB_LIBRARIES)
+# find out the TBB version that is present
+CONFIGURE_FILE(${CMAKE_MODULE_PATH}/ExtractTBBVersion.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/ExtractTBBVersion.cpp @ONLY)
+TRY_RUN(TBB_VERSION_RUN_RESULT TBB_VERSION_COMPILE_RESULT
+        ${CMAKE_BINARY_DIR}
+        ${CMAKE_CURRENT_BINARY_DIR}/ExtractTBBVersion.cpp
+        RUN_OUTPUT_VARIABLE TBB_VERSION)
+MESSAGE(STATUS "TBB_VERSION: ${TBB_VERSION}")
+
+# check if the version is acceptable
+SET(TBB_VERSION_ACCEPTABLE TRUE)
+IF(TBB_FIND_VERSION)
+    
+    MESSAGE(STATUS "TBB version check requested with target version ${TBB_FIND_VERSION}")
+    
+    IF(NOT TBB_VERSION_COMPILE_RESULT)
+        # compiling did not work, no version extracted
+        SET(TBB_VERSION_ACCEPTABLE FALSE)
+    ELSEIF(NOT TBB_VERSION_RUN_RESULT EQUAL 0)
+        # running did not work
+        SET(TBB_VERSION_ACCEPTABLE FALSE)
+    ELSEIF(${TBB_VERSION} VERSION_LESS ${TBB_FIND_VERSION})
+        # wrong version
+        SET(TBB_VERSION_ACCEPTABLE FALSE)
+    ENDIF()
+    
+ENDIF()
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TBB DEFAULT_MSG TBB_INCLUDE_DIRS TBB_LIBRARIES TBB_VERSION_ACCEPTABLE)
 
 # only visible in advanced view
 MARK_AS_ADVANCED(TBB_INCLUDE_DIRS TBB_LIBRARIES TBB_LIBRARY TBBMALLOC_LIBRARY)

53881254c6da738cb1c2a623ce2bea2b009c5951
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2010-11-24 14:40:43 +0000

    return ERRORLEVE from the build script

--- project/build_vs9.bat
+++ project/build_vs9.bat
@@ -7,24 +7,27 @@ call "%VS90COMNTOOLS%/vsvars32.bat"
 cd build
 IF %ERRORLEVEL% NEQ 0 (
 	echo "Could not change to build directory"
-	goto :cleanup
+	goto :error
 )
 
 cmake /G "Visual Studio 9 2008" /DCMAKE_INSTALL_PREFIX=%1 /DCMAKE_BUILD_TYPE=debug ..
 IF %ERRORLEVEL% NEQ 0 (
 	echo "CMake error"
-	goto :cleanup
+	goto :error
 )
 
 msbuild /t:ALL_BUILD /v:d RSC.sln
 IF %ERRORLEVEL% NEQ 0 (
 	echo "Build error"
-	goto :cleanup
+	goto :error
 )
 
 REM TODO unit tests
 
 echo "success"
 
-:cleanup
+exit /b 0
+
+:error
 cd ..
+exit /b 1

39ae145a91bbf4424984a90edd9f2b09fbd54f94
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2010-11-26 09:55:28 +0000

    provide caching capabilities for the init method

--- cmake/CheckInitMethod.cmake
+++ cmake/CheckInitMethod.cmake
@@ -18,48 +18,55 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details. 
 
-MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working")
-TRY_RUN(ATTRIBUTE_CONSTRUCTOR_RESULT COMPILER_COMPILES_ATTRIBUTE_CONSTRUCTOR
-        ${CMAKE_BINARY_DIR}
-        ${CMAKE_MODULE_PATH}/TestAttributeConstructor.cpp
-        RUN_OUTPUT_VARIABLE RUN_OUTPUT)
+IF(INIT_METHOD_CRT OR INIT_METHOD_ATTRIBUTE_CONSTRUCTOR)
+    MESSAGE(STATUS "Using cached init method values")
+ELSE()
+    MESSAGE(STATUS "Determining init method")
 
-IF(ATTRIBUTE_CONSTRUCTOR_RESULT)
-    MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - compiles")
+    MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working")
+    TRY_RUN(ATTRIBUTE_CONSTRUCTOR_RESULT COMPILER_COMPILES_ATTRIBUTE_CONSTRUCTOR
+            ${CMAKE_BINARY_DIR}
+            ${CMAKE_MODULE_PATH}/TestAttributeConstructor.cpp
+            RUN_OUTPUT_VARIABLE RUN_OUTPUT)
     
-    IF(RUN_OUTPUT MATCHES ".*CONSTRUCTOR.*")
-        MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - yes")
-        SET(INIT_METHOD_ATTRIBUTE_CONSTRUCTOR TRUE CACHE BOOL "auto init method that can be used" FORCE)
-    ELSEIF()
-        MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - no")
+    IF(ATTRIBUTE_CONSTRUCTOR_RESULT)
+        MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - compiles")
+        
+        IF(RUN_OUTPUT MATCHES ".*CONSTRUCTOR.*")
+            MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - yes")
+            SET(INIT_METHOD_ATTRIBUTE_CONSTRUCTOR TRUE CACHE BOOL "auto init method that can be used" FORCE)
+        ELSEIF()
+            MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - no")
+            SET(INIT_METHOD_ATTRIBUTE_CONSTRUCTOR FALSE CACHE BOOL "auto init method that can be used" FORCE)
+        ENDIF()
+        
+    ELSE()
+        MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - does not compile")
         SET(INIT_METHOD_ATTRIBUTE_CONSTRUCTOR FALSE CACHE BOOL "auto init method that can be used" FORCE)
     ENDIF()
     
-ELSE()
-    MESSAGE(STATUS "Checking if __attribute__ ((constructor)) is working - does not compile")
-    SET(INIT_METHOD_ATTRIBUTE_CONSTRUCTOR FALSE CACHE BOOL "auto init method that can be used" FORCE)
-ENDIF()
-
-MESSAGE(STATUS "Checking if CRT initialization is working")
-TRY_RUN(CRT_RESULT COMPILER_COMPILES_CRT
-        ${CMAKE_BINARY_DIR}
-        ${CMAKE_MODULE_PATH}/TestCRTInit.cpp
-        RUN_OUTPUT_VARIABLE RUN_OUTPUT)
+    MESSAGE(STATUS "Checking if CRT initialization is working")
+    TRY_RUN(CRT_RESULT COMPILER_COMPILES_CRT
+            ${CMAKE_BINARY_DIR}
+            ${CMAKE_MODULE_PATH}/TestCRTInit.cpp
+            RUN_OUTPUT_VARIABLE RUN_OUTPUT)
+            
+    IF(CRT_RESULT)
+        MESSAGE(STATUS "Checking if CRT initialization is working - compiles")
         
-IF(CRT_RESULT)
-    MESSAGE(STATUS "Checking if CRT initialization is working - compiles")
-    
-    IF(RUN_OUTPUT MATCHES ".*CONSTRUCTOR.*")
-        MESSAGE(STATUS "Checking if CRT initialization is working - yes")
-        SET(INIT_METHOD_CRT TRUE CACHE BOOL "auto init method that can be used" FORCE)
+        IF(RUN_OUTPUT MATCHES ".*CONSTRUCTOR.*")
+            MESSAGE(STATUS "Checking if CRT initialization is working - yes")
+            SET(INIT_METHOD_CRT TRUE CACHE BOOL "auto init method that can be used" FORCE)
+        ELSE()
+            MESSAGE(STATUS "Checking if CRT initialization is working - no")
+            SET(INIT_METHOD_CRT FALSE CACHE BOOL "auto init method that can be used" FORCE)
+        ENDIF()
+        
     ELSE()
-        MESSAGE(STATUS "Checking if CRT initialization is working - no")
+        MESSAGE(STATUS "Checking if CRT initialization is working - does not compile")
         SET(INIT_METHOD_CRT FALSE CACHE BOOL "auto init method that can be used" FORCE)
     ENDIF()
     
-ELSE()
-    MESSAGE(STATUS "Checking if CRT initialization is working - does not compile")
-    SET(INIT_METHOD_CRT FALSE CACHE BOOL "auto init method that can be used" FORCE)
-ENDIF()
+    MARK_AS_ADVANCED(INIT_METHOD_ATTRIBUTE_CONSTRUCTOR INIT_METHOD_CRT)
 
-MARK_AS_ADVANCED(INIT_METHOD_ATTRIBUTE_CONSTRUCTOR INIT_METHOD_CRT)
+ENDIF()

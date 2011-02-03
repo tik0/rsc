26693aa90743c9673a9f48c29e54abdb324c935a
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-02-03 15:39:24 +0000

    Added more missing files in src/rsc/runtime * src/rsc/runtime/ContainerIO.h: new file; interface of io routines   for stl containers * src/rsc/runtime/ContainerIO.cpp: new file; implementation details of   io routines for stl containers

---
+++ src/rsc/runtime/ContainerIO.cpp
@@ -0,0 +1,51 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2010, 2011 Jan Moringen
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "ContainerIO.h"
+
+namespace std {
+
+  namespace detail {
+
+    void
+    pair_style_delete(ios_base::event event_,
+		      ios_base&       stream,
+		      int             index) {
+      if (event_ == ios_base::erase_event
+	  && stream.pword(pair_style::stream_storage))
+	delete reinterpret_cast<pair_style*>
+	  (stream.pword(pair_style::stream_storage));
+    }
+
+    pair_style::pair_style(const string& open_brace,
+			   const string& separator,
+			   const string& close_brace)
+      : open_brace(open_brace), separator(separator), close_brace(close_brace) {
+    }
+
+    const int pair_style::stream_storage = ios_base::xalloc();
+
+  }
+
+  const detail::set_pair_style<detail::pair_style> pair_default;
+  const detail::set_pair_style<detail::pair_style> pair_whitespace = {
+    detail::pair_style("", " ", "")
+  };
+
+}
---
+++ src/rsc/runtime/ContainerIO.h
@@ -0,0 +1,223 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2010, 2011 Jan Moringen
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <valarray>
+
+#include <algorithm>
+#include <iterator>
+
+#include <iostream>
+
+namespace std {
+
+  namespace detail {
+
+    void
+    pair_style_delete(ios_base::event event_,
+		      ios_base&       stream,
+		      int             index);
+
+    struct pair_style {
+      string	       open_brace;
+      string	       separator;
+      string	       close_brace;
+
+      static const int stream_storage;
+
+      pair_style(const string& open_brace  = "(",
+		 const string& separator   = ", ",
+		 const string& close_brace = ")");
+    };
+
+    template <typename T>
+    struct set_pair_style {
+      T value;
+    };
+
+  }
+
+  template <typename Ch,
+	    typename Tr,
+	    typename T>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>&           stream,
+	     const detail::set_pair_style<T>& style) {
+    // Delete old style, if any, and install new style.
+    if (stream.pword(detail::pair_style::stream_storage))
+      delete reinterpret_cast<detail::pair_style*>
+	(stream.pword(detail::pair_style::stream_storage));
+    else
+      stream.register_callback(&detail::pair_style_delete, 0);
+
+    stream.pword(detail::pair_style::stream_storage) = new T(style.value);
+
+    // Return the modified stream.
+    return stream;
+  }
+
+  extern const detail::set_pair_style<detail::pair_style> pair_default;
+  extern const detail::set_pair_style<detail::pair_style> pair_whitespace;
+
+  template <typename Ch,
+	    typename Tr,
+	    typename R,
+	    typename S>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>& stream,
+	     const pair<R, S>&      pair) {
+    // Try to retrieve the installed style implementation. Create one,
+    // if none is installed.
+    if (!stream.pword(detail::pair_style::stream_storage))
+      stream << pair_default;
+
+    detail::pair_style& style = *reinterpret_cast<detail::pair_style*>
+      (stream.pword(detail::pair_style::stream_storage));
+
+    stream << style.open_brace
+	   << pair.first
+	   << style.separator
+	   << pair.second
+	   << style.close_brace;
+    return stream;
+  }
+
+  template <typename Ch,
+	    typename Tr,
+	    typename T>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>& stream,
+	     const vector<T>&       container) {
+    typedef vector<T>				container_type;
+    typedef typename container_type::value_type value_type;
+
+    stream << "#(";
+    if (container.size() >= 1) {
+      copy(container.begin(), container.end() - 1,
+	   ostream_iterator<value_type>(stream, ", "));
+      stream << container.back();
+    }
+    stream << ")";
+    return stream;
+  }
+
+  template <typename Ch,
+	    typename Tr,
+	    typename T>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>& stream,
+	     const list<T>&         container) {
+    typedef list<T>				container_type;
+    typedef typename container_type::value_type value_type;
+
+    stream << "[";
+    if (container.size() >= 1) {
+      copy(container.begin(), --container.end(),
+	   ostream_iterator<value_type>(stream, ", "));
+      stream << container.back();
+    }
+    stream << "]";
+    return stream;
+  }
+
+  template <typename Ch,
+	    typename Tr,
+	    typename T>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>& stream,
+	     const set<T>&          container) {
+    typedef set<T>				container_type;
+    typedef typename container_type::value_type value_type;
+
+    stream << "{";
+    if (container.size() >= 1) {
+      copy(++container.begin(), container.end(),
+	   ostream_iterator<value_type>(stream, ", "));
+      stream << *container.begin();
+    }
+    stream << "}";
+    return stream;
+  }
+
+  template <typename Ch,
+	    typename Tr,
+	    typename R,
+	    typename S>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>& stream,
+	     const map<R, S>&       container) {
+    typedef map<R, S> container_type;
+
+    stream << "{";
+    for (typename container_type::const_iterator it
+	   = container.begin(); it != container.end();) {
+      stream << it->first << ": " << it->second;
+      if (++it != container.end())
+	stream << ", ";
+    }
+    stream << "}";
+    return stream;
+  }
+
+  template <typename Ch,
+	    typename Tr,
+	    typename T>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>& stream,
+	     const valarray<T>&     container) {
+    typedef valarray<T>				container_type;
+    typedef typename container_type::value_type value_type;
+
+    stream << "(";
+    for (unsigned int i = 0; i != container.size(); ++i) {
+      stream << container[i];
+      if (i != container.size() - 1)
+	stream << ", ";
+    }
+    stream << ")";
+    return stream;
+  }
+
+  template <typename Ch,
+	    typename Tr,
+	    typename T>
+  basic_ostream<Ch, Tr>&
+  operator<<(basic_ostream<Ch, Tr>& stream,
+	     const slice_array<T>&  container) {
+    typedef slice_array<T>			container_type;
+    typedef typename container_type::value_type value_type;
+
+    stream << "s(";
+    /*    for (unsigned int i = 0; i != container.size(); ++i) {
+      stream << container[i];
+      if (i != container.size() - 1)
+	stream << ", ";
+	}*/
+    stream << ")";
+    return stream;
+  }
+
+}

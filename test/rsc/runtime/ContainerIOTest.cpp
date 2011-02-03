d3d1527b1ea840396eda3979714906edce7941cd
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-02-03 16:40:44 +0000

    Added unit tests for "runtime" components * test/CMakeLists.txt: added glob pattern to register unit test   sources in test/rsc/runtime * test/rsc/runtime/ContainerIOTest.cpp: new file; unit tests for stl   container io routines * test/rsc/runtime/DemangleTest.cpp: new file; unit tests for   demangling functions * test/rsc/runtime/TypeStringToolsTest.cpp: new file; unit tests for   type string tools

---
+++ test/rsc/runtime/ContainerIOTest.cpp
@@ -0,0 +1,77 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2011 Jan Moringen
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include <utility>
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+
+#include <boost/lexical_cast.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace rsc::runtime;
+
+/*! \brief
+ *
+ */
+class ContainerIOTest : public ::testing::Test {
+public:
+  pair<double, string> pair(5.0, "a");
+  list<int>            list;
+  vector<int>          vector;
+  set<int>             set;
+  map<string, double>  map;
+
+  virtual void
+  SetUp() {
+    this->list.clear();
+    this->list.push_back(1);
+    this->list.push_back(2);
+    this->list.push_back(3);
+    this->list.push_back(4);
+
+    this->vector.clear();
+    this->vector.push_back(1);
+    this->vector.push_back(2);
+    this->vector.push_back(3);
+    this->vector.push_back(4);
+
+    this->set.clear();
+    this->set.insert(1);
+    this->set.insert(2);
+    this->set.insert(3);
+    this->set.insert(4);
+
+    this->map.clear();
+    this->map.insert(make_pair("a", 1.0));
+    this->map.insert(make_pair("b", 2.0));
+    this->map.insert(make_pair("c", 3.0));
+  }
+};
+
+TEST(ContainerIOTest, testOutput) {
+  EXPECT_EQ(lexical_cast<std::string>(this->list),
+	    "[1, 2, 3, 4]");
+  EXPECT_EQ(demangle(typeid(int).name()),              "int");
+  EXPECT_EQ(demangle(typeid(std::string).name()),      "std::string");
+  EXPECT_EQ(demangle(typeid(std::vector<int>).name()), "std::vector<int>");
+}
---
+++ test/rsc/runtime/TypeStringToolsTest.cpp
@@ -0,0 +1,65 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2011 Jan Moringen
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include <rsc/runtime/TypeStringTools.h>
+
+using namespace std;
+using namespace rsc::runtime;
+
+TEST(TypeStringToolsTest, test_type_name) {
+  EXPECT_EQ(type_name(typeid(bool)),   "bool");
+  EXPECT_EQ(type_name(typeid(int)),    "int");
+  EXPECT_EQ(type_name(typeid(string)), "std::string");
+
+  EXPECT_EQ(type_name<bool>(),         "bool");
+  EXPECT_EQ(type_name<int>(),          "int");
+  EXPECT_EQ(type_name<string>(),       "std::string");
+
+  EXPECT_EQ(type_name(true),           "bool");
+  EXPECT_EQ(type_name(1),              "int");
+  EXPECT_EQ(type_name(string("bla")),  "std::string");
+}
+
+TEST(TypeStringToolsTest, test_type_string) {
+  EXPECT_EQ(type_string("known type %1%",
+			"unknown type",
+			true),
+	    "known type 1");
+  EXPECT_EQ(type_string("known type %1%",
+			"unknown type",
+			5),
+	    "known type 5");
+  EXPECT_EQ(type_string("known type %1%",
+			"unknown type",
+			string("test")),
+	    "known type test");
+  EXPECT_EQ(type_string("known type %1%",
+			"unknown type",
+			vector<int>()),
+	    "known type #()");
+  EXPECT_EQ(type_string("known type %1%",
+			"unknown type",
+			vector< vector<int> >()),
+	    "known type #()");
+}

4b571205a2a1a755aa0a8be3978a75d7a867dfd3
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-02-07 23:17:09 +0000

    improve finding TBB

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -48,81 +48,85 @@ ENDIF()
 
 SET(TBB_LIBRARIES "")
 
-# find out if this is a visual studio build and if so which version
-STRING(REGEX MATCH "Visual Studio ([0-9]+)" VMATCH ${CMAKE_GENERATOR})
-IF(VMATCH)
-    STRING(LENGTH ${VMATCH} MATCH_LENGTH)
-    STRING(LENGTH "Visual Studio " PREFIX_LENGTH)
-    MATH(EXPR VERSION_LENGTH "${MATCH_LENGTH} - ${PREFIX_LENGTH}")
-    STRING(SUBSTRING ${VMATCH} ${PREFIX_LENGTH} ${VERSION_LENGTH} VC_VERSION)
-    MESSAGE("Visual studio version: ${VC_VERSION}")
+SET(TBB_BITS "32")
+IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	SET(TBB_BITS "64")
 ENDIF()
 
-# decide if 32 or 64 bits
-# TODO jwienke: do this automatically by using one of cmake's size check methods
-IF(NOT TBB_BITS)
-    MESSAGE(STATUS "Assuming 32 bit system")
-    SET(TBB_BITS "32")
+SET(TBB_FOLDER_SUFFIX release)
+SET(TBB_VC_FOLDER Release)
+IF(TBB_DEBUG)
+	SET(TBB_FOLDER_SUFFIX debug)
+	SET(TBB_VC_FOLDER Debug)
 ENDIF()
-
-SET(WIN_LIB_PATH "${TBB_ROOT}/lib/ia${TBB_BITS}/vc${VC_VERSION}")
-SET(TBB_LIBRARY_DIRS "${TBB_ROOT}/bin/ia${TBB_BITS}/vc${VC_VERSION}")
+SET(TBB_LIBRARY_HINTS ${TBB_ROOT}/lib
+                      ${TBB_ROOT}
+                      ${TBB_LIB_PATH}
+					  "${TBB_ROOT}/lib/ia${TBB_BITS}/vc8"
+                      "${TBB_ROOT}/lib/ia${TBB_BITS}/vc9"
+					  "${TBB_ROOT}/lib/ia${TBB_BITS}/vc10"
+					  "${TBB_ROOT}/build/windows_ia${TBB_BITS}_cl_vc8_${TBB_FOLDER_SUFFIX}"
+                      "${TBB_ROOT}/build/windows_ia${TBB_BITS}_cl_vc9_${TBB_FOLDER_SUFFIX}"
+					  "${TBB_ROOT}/build/windows_ia${TBB_BITS}_cl_vc10_${TBB_FOLDER_SUFFIX}"
+					  "${TBB_ROOT}/build/vsproject/ia${TBB_BITS}/${TBB_VC_FOLDER}"
+                      ${CMAKE_INSTALL_PREFIX}
+                      ${CMAKE_INSTALL_PREFIX}/lib)
 
 FIND_LIBRARY(TBB_LIBRARY
-             HINTS ${TBB_ROOT}/lib
-                   ${TBB_ROOT}
-                   ${TBB_LIB_PATH}
-                   ${WIN_LIB_PATH}
-                   ${CMAKE_INSTALL_PREFIX}
-                   ${CMAKE_INSTALL_PREFIX}/lib
+             HINTS ${TBB_LIBRARY_HINTS}
              NAMES ${TBB_LIB_NAME})
 FIND_LIBRARY(TBBMALLOC_LIBRARY
-             HINTS ${TBB_ROOT}/lib
-                   ${TBB_ROOT}
-                   ${TBB_LIB_PATH}
-                   ${WIN_LIB_PATH}
-                   ${CMAKE_INSTALL_PREFIX}
-                   ${CMAKE_INSTALL_PREFIX}/lib
+             HINTS ${TBB_LIBRARY_HINTS}
              NAMES ${TBBMALLOC_LIB_NAME})
-             
+			 
 IF(TBB_LIBRARY AND TBBMALLOC_LIBRARY)
     SET(TBB_LIBRARIES ${TBB_LIBRARY} ${TBBMALLOC_LIBRARY})
 ENDIF()
-             
+
 # post-process inlude path
 IF(TBB_INCLUDE_DIRS)
     STRING(REGEX REPLACE tbb/?$$ "" TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIRS})
 ENDIF()
 SET(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIRS} CACHE PATH "TBB include dirs" FORCE)
 
-# find out the TBB version that is present
-CONFIGURE_FILE("${FIND_TBB_MODULE_DIR}/ExtractTBBVersion.cpp.in" "${CMAKE_CURRENT_BINARY_DIR}/ExtractTBBVersion.cpp" @ONLY)
-TRY_RUN(TBB_VERSION_RUN_RESULT TBB_VERSION_COMPILE_RESULT
-        "${CMAKE_BINARY_DIR}"
-        "${CMAKE_CURRENT_BINARY_DIR}/ExtractTBBVersion.cpp"
-        RUN_OUTPUT_VARIABLE TBB_VERSION)
-MESSAGE(STATUS "TBB_VERSION: ${TBB_VERSION}")
+IF(NOT TBB_VERSION_ACCEPTABLE)
 
-# check if the version is acceptable
-SET(TBB_VERSION_ACCEPTABLE TRUE)
-IF(TBB_FIND_VERSION)
-    
-    MESSAGE(STATUS "TBB version check requested with target version ${TBB_FIND_VERSION}")
-    
-    IF(NOT TBB_VERSION_COMPILE_RESULT)
-        # compiling did not work, no version extracted
-        SET(TBB_VERSION_ACCEPTABLE FALSE)
-    ELSEIF(NOT TBB_VERSION_RUN_RESULT EQUAL 0)
-        # running did not work
-        SET(TBB_VERSION_ACCEPTABLE FALSE)
-    ELSEIF(${TBB_VERSION} VERSION_LESS ${TBB_FIND_VERSION})
-        # wrong version
-        SET(TBB_VERSION_ACCEPTABLE FALSE)
-    ENDIF()
-    
+	# find out the TBB version that is present
+	CONFIGURE_FILE("${FIND_TBB_MODULE_DIR}/ExtractTBBVersion.cpp.in" "${CMAKE_CURRENT_BINARY_DIR}/ExtractTBBVersion.cpp" @ONLY)
+	TRY_RUN(TBB_VERSION_RUN_RESULT TBB_VERSION_COMPILE_RESULT
+			"${CMAKE_BINARY_DIR}"
+			"${CMAKE_CURRENT_BINARY_DIR}/ExtractTBBVersion.cpp"
+			CMAKE_FLAGS "-DLINK_LIBRARIES:STRING=${TBB_LIBRARY}"
+			RUN_OUTPUT_VARIABLE TBB_VERSION
+			COMPILE_OUTPUT_VARIABLE TBB_VERSION_COMPILE_OUT)
+	#MESSAGE("Version check compiled: ${TBB_VERSION_COMPILE_RESULT} - ${TBB_VERSION_COMPILE_OUT}")
+	MESSAGE(STATUS "TBB_VERSION: ${TBB_VERSION}")
+
+	# check if the version is acceptable
+	SET(TBB_VERSION_ACCEPTABLE TRUE)
+	IF(TBB_FIND_VERSION)
+		
+		MESSAGE(STATUS "TBB version check requested with target version ${TBB_FIND_VERSION}")
+		
+		IF(NOT TBB_VERSION_COMPILE_RESULT)
+			# compiling did not work, no version extracted
+			SET(TBB_VERSION_ACCEPTABLE FALSE)
+		ELSEIF(NOT TBB_VERSION_RUN_RESULT EQUAL 0)
+			# running did not work
+			SET(TBB_VERSION_ACCEPTABLE FALSE)
+		ELSEIF(${TBB_VERSION} VERSION_LESS ${TBB_FIND_VERSION})
+			# wrong version
+			SET(TBB_VERSION_ACCEPTABLE FALSE)
+		ELSE()
+		    # store we do not want to repeat this check...
+			SET(TBB_VERSION_ACCEPTABLE TRUE CACHE BOOL "TBB version was checked and is sufficient" FORCE)
+		ENDIF()
+		
+	ENDIF()
+
 ENDIF()
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(TBB DEFAULT_MSG TBB_INCLUDE_DIRS TBB_LIBRARIES TBB_VERSION_ACCEPTABLE)
 
 # only visible in advanced view
-MARK_AS_ADVANCED(TBB_INCLUDE_DIRS TBB_LIBRARIES TBB_LIBRARY TBBMALLOC_LIBRARY)
+MARK_AS_ADVANCED(TBB_INCLUDE_DIRS TBB_LIBRARIES TBB_LIBRARY TBBMALLOC_LIBRARY TBB_VERSION_ACCEPTABLE)

7c1f55ceb0cb23388f6953984fe34e31266fa8c4
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-10-12 13:48:41 +0000

    fix wrong install prefix variablen name

--- cmake/FindBoostUUID.cmake
+++ cmake/FindBoostUUID.cmake
@@ -27,7 +27,7 @@ INCLUDE(FindPackageHandleStandardArgs)
 
 FIND_PATH(BOOSTUUID_INCLUDE_DIRS
           NAMES uuid.hpp
-          HINTS ${BOOSTUUID_ROOT} ${BOOST_ROOT} ${CMAKE_INSTALL_PATH}
+          HINTS ${BOOSTUUID_ROOT} ${BOOST_ROOT} ${CMAKE_INSTALL_PREFIX}
           PATH_SUFFIXES include/boost/uuid)
 
 # post-process include path

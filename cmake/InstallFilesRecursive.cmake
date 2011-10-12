3b9cbe9009329bbc591744addd470e02d4848d72
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-10-12 11:43:11 +0000

    add documentation and proper license

--- cmake/InstallFilesRecursive.cmake
+++ cmake/InstallFilesRecursive.cmake
@@ -1,3 +1,40 @@
+# - Install files recursively using their header structure in the source tree
+# This function install files using their header structure in the source tree.
+# This is e.g. usefull to install header files with their structure. Automatic
+# support for not mixing up the path logic is provided for generated files
+# if they are located in CMAKE_CURRENT_BINARY dir. Otherwise
+# CMAKE_CURRENT_SOURCE_DIR is assumed as the absolute location for the files to
+# install. 
+#
+# INSTALL_FILES_RECURSIVE(DESTINATION FILE_LIST)
+#
+#     DESTINATION: destination folder under which the structure of the files to
+#                  install is mirrored. INSTALL is used, hence relative
+#                  locations will be relative to CMAKE_INSTALL_PREFIX.
+#     FILE_LIST: list of files to install
+#
+# Exmaple:
+# INSTALL_FILES_RECURSIVE(include "rsc/foo.h"
+#                                 "rsc/test/bla.h"
+#                                 "${CMAKE_CURRENT_BINARY_DIR}/rsc/narf/damn/xxx.h")
+# will install:
+#   ${CMAKE_INSTALL_PREFIX}/include/rsc/foo.h
+#   ${CMAKE_INSTALL_PREFIX}/include/rsc/test/bla.h
+#   ${CMAKE_INSTALL_PREFIX}/include/rsc/narf/damn/xxx.h
+#
+# Copyright (C) 2010 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 
 MACRO(INSTALL_FILES_RECURSIVE DESTINATION FILE_LIST)
 

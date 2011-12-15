fcb28832e9252393fd36a24743dd2e9e589a3af3
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-12-15 08:42:14 +0000

    missing files
    refs #101

---
+++ src/rsc/logging/LoggerTreeNode.cpp
@@ -0,0 +1,200 @@
+/* ============================================================
+ *
+ * This file is a part of the RSC project.
+ *
+ * Copyright (C) 2011 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "LoggerTreeNode.h"
+
+#include <boost/format.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include <rsc/runtime/ContainerIO.h>
+
+using namespace std;
+
+namespace rsc {
+namespace logging {
+
+LoggerTreeNode::LoggerTreeNode(const string& name, LoggerTreeNodeWeakPtr parent) :
+        name(name), parent(parent) {
+}
+
+LoggerTreeNode::LoggerTreeNode(const string& name, LoggerProxyPtr loggerProxy,
+        LoggerTreeNodeWeakPtr parent) :
+        name(name), loggerProxy(loggerProxy), parent(parent) {
+}
+
+LoggerTreeNodeWeakPtr LoggerTreeNode::getParent() const {
+    return parent;
+}
+
+string LoggerTreeNode::getName() const {
+    return name;
+}
+
+LoggerProxyPtr LoggerTreeNode::getLoggerProxy() const {
+    return loggerProxy;
+}
+
+void LoggerTreeNode::setLoggerProxy(LoggerProxyPtr loggerProxy) {
+    this->loggerProxy = loggerProxy;
+}
+
+bool LoggerTreeNode::addChild(LoggerTreeNodePtr child) {
+    // we rely on the fact that map only inserts if not present
+    return children.insert(make_pair(child->getName(), child)).second;
+}
+
+LoggerTreeNodePtr LoggerTreeNode::addChildren(const NamePath& path,
+        CreateFunction createFn, const NamePath &processedPath) {
+
+    if (path.size() == 0) {
+        throw invalid_argument("Empty path given");
+    }
+
+    const string directChildName = path.front();
+    NamePath subPath = path;
+    subPath.erase(subPath.begin());
+    NamePath childPath = processedPath;
+    childPath.push_back(directChildName);
+
+    if (!children.count(path.front())) {
+        children[path.front()] = LoggerTreeNodePtr(
+                new LoggerTreeNode(path.front(), shared_from_this()));
+        children[path.front()]->setLoggerProxy(
+                createFn(childPath, children[directChildName]));
+    }
+
+    if (path.size() > 1) {
+        return children[directChildName]->addChildren(subPath, createFn,
+                childPath);
+    } else {
+        return children[directChildName];
+    }
+
+}
+
+LoggerTreeNode::Visitor::~Visitor() {
+}
+
+void LoggerTreeNode::visit(VisitorPtr visitor, const NamePath& thisPath) {
+
+    for (map<string, LoggerTreeNodePtr>::const_iterator it = children.begin();
+            it != children.end(); ++it) {
+
+        NamePath childPath = thisPath;
+        childPath.push_back(it->first);
+
+        bool descend = visitor->visit(childPath, it->second,
+                loggerProxy->getLogger()->getLevel());
+        if (descend) {
+            it->second->visit(visitor, childPath);
+        }
+
+    }
+
+}
+
+bool LoggerTreeNode::hasChild(const string& name) const {
+    return children.count(name) == 1;
+}
+
+bool LoggerTreeNode::hasChild(const NamePath& path) const {
+    if (path.size() == 0) {
+        throw invalid_argument("Empty name path given");
+    }
+    map<string, LoggerTreeNodePtr>::const_iterator childIt = children.find(
+            path.front());
+    if (childIt == children.end()) {
+        return false;
+    }
+    NamePath subPath = path;
+    subPath.erase(subPath.begin());
+    return childIt->second->hasChild(subPath);
+}
+
+LoggerTreeNodePtr LoggerTreeNode::getChild(const string& name) const {
+    map<string, LoggerTreeNodePtr>::const_iterator it = children.find(name);
+    if (it == children.end()) {
+        throw invalid_argument(
+                boost::str(
+                        boost::format("No direct child with name %1 exists.")
+                                % name));
+    }
+    return it->second;
+}
+
+LoggerTreeNodePtr LoggerTreeNode::getChild(const NamePath& path) const {
+    if (path.size() == 0) {
+        throw invalid_argument("Empty name path given");
+    }
+    map<string, LoggerTreeNodePtr>::const_iterator childIt = children.find(
+            path.front());
+    if (childIt == children.end()) {
+        throw invalid_argument(
+                boost::str(
+                        boost::format("No direct child with name %1 exists.")
+                                % name));
+    }
+    NamePath subPath = path;
+    subPath.erase(subPath.begin());
+    return childIt->second->getChild(subPath);
+}
+
+void LoggerTreeNode::clearChildren() {
+    children.clear();
+}
+
+LoggerTreeNode::NamePath LoggerTreeNode::nameToPath(const string& name) {
+    if (name.empty()) {
+        return NamePath();
+    }
+    NamePath path;
+    // TODO add consistency checks
+    boost::algorithm::split(path, name, boost::algorithm::is_any_of("."));
+    return path;
+}
+
+string LoggerTreeNode::pathToName(const NamePath& path) {
+    stringstream s;
+    for (NamePath::const_iterator it = path.begin(); it != path.end(); ++it) {
+        s << *it;
+        if (it != --path.end()) {
+            s << ".";
+        }
+    }
+    return s.str();
+}
+
+boost::shared_ptr<Logger::Level> LoggerTreeNode::getAssignedLevel() const {
+    return assignedLevel;
+}
+
+void LoggerTreeNode::setAssignedLevel(boost::shared_ptr<Logger::Level> level) {
+    this->assignedLevel = level;
+}
+
+void LoggerTreeNode::setAssignedLevel(const Logger::Level& level) {
+    assignedLevel.reset(new Logger::Level(level));
+}
+
+bool LoggerTreeNode::hasAssignedLevel() const {
+    return assignedLevel;
+}
+
+}
+}
---
+++ src/rsc/logging/LoggerTreeNode.h
@@ -0,0 +1,172 @@
+/* ============================================================
+ *
+ * This file is a part of the RSC project.
+ *
+ * Copyright (C) 2011 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "LoggerProxy.h"
+
+namespace rsc {
+namespace logging {
+
+class LoggerTreeNode;
+typedef boost::shared_ptr<LoggerTreeNode> LoggerTreeNodePtr;
+typedef boost::weak_ptr<LoggerTreeNode> LoggerTreeNodeWeakPtr;
+
+/**
+ * A simple tree representation for loggers.
+ *
+ * We do not use any special library but create our own one because all publicly
+ * available libs like BGL are too big for this purpose and BGL is often not
+ * included in boost distributions.
+ *
+ * @author jwienke
+ */
+class LoggerTreeNode: public boost::enable_shared_from_this<LoggerTreeNode> {
+public:
+
+    /**
+     * A unique representation of a name. From the string representation this
+     * is the name split at each '.' char. Empty vector means root logger.
+     */
+    typedef std::vector<std::string> NamePath;
+
+    /**
+     * Creates a new node without an assigned LoggerProxy. This needs to be set
+     * afterwards with #setLoggerProxy.
+     *
+     * @param name name of the current path element, i.e. not the full path
+     * @param parent parent tree node
+     */
+    LoggerTreeNode(const std::string& name, LoggerTreeNodeWeakPtr parent);
+
+    /**
+     * Creates a new node with a LoggerProxy assigned.
+     *
+     * @param name name of the current path element, i.e. not the full path
+     * @param loggerProxy assigned logger proxy
+     * @param parent parent tree node
+     */
+    LoggerTreeNode(const std::string& name, LoggerProxyPtr loggerProxy,
+            LoggerTreeNodeWeakPtr parent);
+
+    LoggerTreeNodeWeakPtr getParent() const;
+
+    std::string getName() const;
+
+    LoggerProxyPtr getLoggerProxy() const;
+
+    void setLoggerProxy(LoggerProxyPtr loggerProxy);
+
+    /**
+     * Adds a child if it does not exist so far.
+     *
+     * @param child child to add
+     * @return @c true if the new child was added, @c false if it already exited.
+     */
+    bool addChild(LoggerTreeNodePtr child);
+
+    typedef boost::function<
+            LoggerProxyPtr(const NamePath& name, LoggerTreeNodePtr node)> CreateFunction;
+
+    /**
+     * Retrieves an (indirect) child and creates required ancestors of this
+     * child using a custom callback method.
+     *
+     * @param path path of the child starting from this node
+     * @param createFn function used to create new children
+     * @param processedPath the path of this node. Passed to the create function
+     * @return the deepest child in the path
+     */
+    LoggerTreeNodePtr addChildren(const NamePath& path,
+            CreateFunction createFn, const NamePath &processedPath = NamePath());
+
+    /**
+     * Visitor interface to operate on the tree.
+     *
+     * @author jwienke
+     */
+    class Visitor {
+    public:
+        virtual ~Visitor();
+
+        /**
+         * Called for each node in the tree.
+         *
+         * @param path current path in the tree
+         * @param node currently visited node
+         * @param parentLevel direct or derived level of the parent node
+         * @return @true to descend into this node's children, else @c false
+         */
+        virtual bool visit(const NamePath& path, LoggerTreeNodePtr node, const Logger::Level& parentLevel) = 0;
+
+    };
+    typedef boost::shared_ptr<Visitor> VisitorPtr;
+
+    /**
+     * Visits every sub-node excluding this node. Depth-first strategy is used.
+     *
+     * @param visitor visitor to use
+     */
+    void visit(VisitorPtr visitor, const NamePath& thisPath = NamePath());
+
+    bool hasChild(const std::string& name) const;
+
+    bool hasChild(const NamePath& path) const;
+
+    LoggerTreeNodePtr getChild(const std::string& name) const;
+
+    LoggerTreeNodePtr getChild(const NamePath& path) const;
+
+    void clearChildren();
+
+    static NamePath nameToPath(const std::string& name);
+    static std::string pathToName(const NamePath& path);
+
+    boost::shared_ptr<Logger::Level> getAssignedLevel() const;
+    void setAssignedLevel(boost::shared_ptr<Logger::Level> level);
+    void setAssignedLevel(const Logger::Level& level);
+    bool hasAssignedLevel() const;
+
+private:
+
+    /**
+     * New name part of this node. Somehow mirrored from loggerProxy because we
+     * do not want to rely on the custom logger implementation correctness.
+     */
+    std::string name;
+    LoggerProxyPtr loggerProxy;
+
+    LoggerTreeNodeWeakPtr parent;
+    std::map<std::string, LoggerTreeNodePtr> children;
+
+    boost::shared_ptr<Logger::Level> assignedLevel;
+
+};
+
+}
+}
+

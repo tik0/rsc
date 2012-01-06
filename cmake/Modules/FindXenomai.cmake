6a7362d305f0b530b2895298c7a49173353b8e69
Arne Nordmann <anordman@techfak.uni-bielefeld.de>
2012-01-06 10:17:19 +0000

    Licensing of cmake module and toolchain file

--- cmake/Modules/FindXenomai.cmake
+++ cmake/Modules/FindXenomai.cmake
@@ -1,3 +1,38 @@
+# - Try to find the Xenomai Real-Time 
+# Will define:
+#
+# XENOMAI_INCLUDE_DIR - Include directories needed to use the C++ driver
+# XENOMAI_INCLUDE_POSIX_DIR - Include directories for POSIX systems
+# XENOMAI_POSIX_WRAPPERS - Directories containing the libraries (win)
+# XENOMAI_LIBRARY_XENOMAI - Xenomai library 
+# XENOMAI_LIBRARY_NATIVE - Native library
+# XENOMAI_LIBRARY_PTHREAD_RT - 
+# XENOMAI_LIBRARY_RTDM - 
+# XENOMAI_DEFINITIONS - Xenomai definitions / compiler flags
+#
+# Possible hints:
+# XENOMAI_ROOT - root directory of the TBB installation
+#
+# Copyright (C) 2010 by Arne Nordmann <anordman at cor-lab dot de>
+#
+# This file may be licensed under the terms of the
+# GNU Lesser General Public License Version 3 (the ``LGPL''),
+# or (at your option) any later version.
+#
+# Software distributed under the License is distributed
+# on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+# express or implied. See the LGPL for the specific language
+# governing rights and limitations.
+#
+# You should have received a copy of the LGPL along with this
+# program. If not, go to http://www.gnu.org/licenses/lgpl.html
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# The development of this software was supported by:
+#   CoR-Lab, Research Institute for Cognition and Robotics
+#     Bielefeld University
+
 IF(UNIX)
 
 	# Search Path
@@ -29,15 +64,12 @@ IF(UNIX)
 		# Find posix wrappers
 		FIND_FILE(XENOMAI_POSIX_WRAPPERS lib/posix.wrappers ${XENOMAI_SEARCH_PATH})
 
-		# Set the linker flags
-		SET(XENOMAI_EXE_LINKER_FLAGS "-Wl,@${XENOMAI_POSIX_WRAPPERS}")
-
 		# Add compile/preprocess options
 		SET(XENOMAI_DEFINITIONS "-D_GNU_SOURCE -D_REENTRANT -Wall -pipe -D__XENO__")
 	
 	ENDIF(XENOMAI_DIR)
-	
-	INCLUDE(FindPackageHandleStandardArgs)
-	FIND_PACKAGE_HANDLE_STANDARD_ARGS(Xenomai DEFAULT_MSG XENOMAI_DIR)
 
 ENDIF(UNIX)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Xenomai DEFAULT_MSG XENOMAI_DIR)
--- cmake/Toolchains/Oncilla_RB110.cmake
+++ cmake/Toolchains/Oncilla_RB110.cmake
@@ -1,4 +1,25 @@
-MESSAGE(STATUS "Loading toolchain for Oncilla system")
+# - Toolchain file for AMARSi Oncilla Platform (RoBoard RB 110) 
+# Will adjust compiler flags and target architecture.
+#
+# Copyright (C) 2010 by Arne Nordmann <anordman at cor-lab dot de>
+#
+# This file may be licensed under the terms of the
+# GNU Lesser General Public License Version 3 (the ``LGPL''),
+# or (at your option) any later version.
+#
+# Software distributed under the License is distributed
+# on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+# express or implied. See the LGPL for the specific language
+# governing rights and limitations.
+#
+# You should have received a copy of the LGPL along with this
+# program. If not, go to http://www.gnu.org/licenses/lgpl.html
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# The development of this software was supported by:
+#   CoR-Lab, Research Institute for Cognition and Robotics
+#     Bielefeld UniversityMESSAGE(STATUS "Loading toolchain for Oncilla system")
 
 # System description
 SET(CMAKE_SYSTEM_NAME "Linux")

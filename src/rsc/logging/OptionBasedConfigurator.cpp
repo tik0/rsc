6f11e0eda53463067a384804570775c2c3566ff1
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2012-03-02 09:03:40 +0000

    Add an OptionHandler which can be used to configure the levels of the logging system.
    refs #772

---
+++ src/rsc/logging/OptionBasedConfigurator.cpp
@@ -0,0 +1,132 @@
+/* ============================================================
+ *
+ * This file is a part of the RSC project.
+ *
+ * Copyright (C) 2012 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include "OptionBasedConfigurator.h"
+
+#include "Logger.h"
+
+using namespace std;
+using namespace rsc::config;
+
+namespace rsc {
+namespace logging {
+
+OptionBasedConfigurator::OptionBasedConfigurator(
+        const vector<string>& rootOption) :
+        rootOption(rootOption) {
+}
+
+OptionBasedConfigurator::~OptionBasedConfigurator() {
+}
+
+vector<string> OptionBasedConfigurator::getDefaultRootOption() {
+    vector<string> root;
+    root.push_back("rsc");
+    root.push_back("logging");
+    return root;
+}
+
+bool OptionBasedConfigurator::keyStartWithRoot(
+        const vector<string>& key) const {
+
+    if (key.size() < rootOption.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < rootOption.size(); ++i) {
+        if (rootOption[i] != key[i]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+string OptionBasedConfigurator::loggerNameFromKey(
+        const vector<string>& key) const {
+
+    string name;
+    // the last fragment must be the setting, do not include in the name
+    for (vector<string>::const_iterator keyIt = key.begin() + rootOption.size();
+            keyIt != key.end() - 1; ++keyIt) {
+
+        name += "." + *keyIt;
+
+    }
+
+    if (name.empty()) {
+        return name;
+    } else {
+        return name.substr(1, name.size() - 1);
+    }
+
+}
+
+void OptionBasedConfigurator::handleOption(const vector<string>& key,
+        const string& value) {
+
+    cout << value << endl;
+
+    if (!keyStartWithRoot(key)) {
+        return;
+    }
+
+    // something can only be a valid key for the configuration if there is a
+    // chance that a special configuration key is at the end. Hence, sufficient
+    // length is required
+    if (key.size() <= rootOption.size()) {
+        return;
+    }
+
+    LoggerPtr logger = Logger::getLogger(loggerNameFromKey(key));
+    cout << logger->getName() << endl;
+
+    string setting = key.back();
+    if (setting == "LEVEL") {
+        if (value == "ALL") {
+            logger->setLevel(Logger::LEVEL_ALL);
+        } else if (value == "TRACE") {
+            logger->setLevel(Logger::LEVEL_TRACE);
+        } else if (value == "DEBUG") {
+            logger->setLevel(Logger::LEVEL_DEBUG);
+        } else if (value == "INFO") {
+            logger->setLevel(Logger::LEVEL_INFO);
+        } else if (value == "WARN") {
+            logger->setLevel(Logger::LEVEL_WARN);
+        } else if (value == "ERROR") {
+            logger->setLevel(Logger::LEVEL_ERROR);
+        } else if (value == "FATAL") {
+            logger->setLevel(Logger::LEVEL_FATAL);
+        } else if (value == "OFF") {
+            logger->setLevel(Logger::LEVEL_OFF);
+        }
+    }
+
+}
+
+}
+}
---
+++ test/rsc/logging/OptionBasedConfiguratorTest.cpp
@@ -0,0 +1,79 @@
+/* ============================================================
+ *
+ * This file is a part of RSC project
+ *
+ * Copyright (C) 2012 by Johannes Wienke <jwienke at techfak dot uni-bielefeld dot de>
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include <stdexcept>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+#include <gtest/gtest.h>
+
+#include "rsc/config/ConfigFileSource.h"
+#include "rsc/logging/LoggerFactory.h"
+#include "rsc/logging/OptionBasedConfigurator.h"
+
+#include "testconfig.h"
+
+using namespace std;
+using namespace rsc;
+using namespace rsc::config;
+using namespace rsc::logging;
+
+TEST(OptionBasedConfiguratorTest, testConfigFromFile)
+{
+
+    LoggerFactory::getInstance().clearKnownLoggers();
+
+    OptionBasedConfigurator configurator;
+    boost::filesystem::ifstream stream(TEST_ROOT + "/rsc/logging/logging.config");
+    ASSERT_TRUE(stream);
+    ConfigFileSource(stream).provideOptions(configurator);
+
+    EXPECT_EQ(Logger::LEVEL_TRACE, Logger::getLogger("")->getLevel());
+    EXPECT_EQ(Logger::LEVEL_DEBUG, Logger::getLogger("sub.logger")->getLevel());
+    EXPECT_EQ(Logger::LEVEL_TRACE, Logger::getLogger("another.logger")->getLevel());
+    EXPECT_EQ(Logger::LEVEL_TRACE, Logger::getLogger("sub.foo")->getLevel());
+
+}
+
+TEST(OptionBasedConfiguratorTest, testDifferentRoot)
+{
+
+    LoggerFactory::getInstance().clearKnownLoggers();
+
+    vector<string> root;
+    root.push_back("a");
+    root.push_back("new");
+    root.push_back("root");
+    OptionBasedConfigurator configurator(root);
+    boost::filesystem::ifstream stream(TEST_ROOT + "/rsc/logging/logging.config");
+    ASSERT_TRUE(stream);
+    ConfigFileSource(stream).provideOptions(configurator);
+
+    EXPECT_EQ(Logger::LEVEL_OFF, Logger::getLogger("sub.logger")->getLevel());
+
+}

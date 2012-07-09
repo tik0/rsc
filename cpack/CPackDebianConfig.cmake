32091e47de8f4a1ef18f2a88574cd22d35814fdd
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-07-09 11:26:38 +0000

    Fixed some Debian policy issues in cpack/CPackDebianConfig.cmake * cpack/CPackDebianConfig.cmake: fixed maintainer name/address; fixed   long description; enabled stripping of executables and shared   objects

--- cpack/CPackDebianConfig.cmake
+++ cpack/CPackDebianConfig.cmake
@@ -1,7 +1,17 @@
-
 IF(NOT CMAKE_SYSTEM_NAME STREQUAL "Linux")
     MESSAGE(FATAL_ERROR "Cannot configure CPack to generate Debian packages on non-linux systems.")
 ENDIF()
+
+# To create a proper Debian/Ubuntu package, the following CMake
+# options should be used:
+
+# SET(BUILD_TESTS OFF)
+# SET(BUILD_EXAMPLES OFF)
+
+# SET(CMAKE_BUILD_TYPE Release)
+SET(CPACK_STRIP_FILES TRUE)
+
+# Actual packaging options
 
 INCLUDE(CheckLSBTypes)
 
@@ -12,8 +22,11 @@ SET(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}${VERSION_SUFFIX}-${CPACK_PACK
 SET(CPACK_GENERATOR "DEB")
 SET(CPACK_DEBIAN_PACKAGE_NAME "rsc${VERSION_SUFFIX}")
 SET(CPACK_DEBIAN_PACKAGE_VERSION ${CPACK_PACKAGE_VERSION})
-SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "Sebastian Wrede (swrede@techfak.uni-bielefeld.de)")
-SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "RSC is a project focused on all aspects of reusable library functions in robotics systems. RSC is meant to improve the C++ std lib for robotic systems without pulling in many additional dependencies.")
+SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "Sebastian Wrede <swrede@techfak.uni-bielefeld.de>")
+SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Library of reusable C++ and CMake code.
+ RSC is a project focused on all aspects of reusable library functions
+ in robotics systems. RSC is meant to improve the C++ std lib for
+ robotic systems without pulling in many additional dependencies.")
 SET(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 SET(CPACK_DEBIAN_PACKAGE_SECTION "devel")
 SET(CPACK_DEBIAN_PACKAGE_CONFLICTS "rsc0.6, rsc0.5, rsc0.4")

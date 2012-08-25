a27c0789b6536f2e2e1ab43ac6b380dbbea44550
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-08-26 01:03:19 +0200

    Changed Debian package filename {rsc -> librsb}....deb * CMakeLists.txt: define generic CPACK_PACKAGE_VERSION * cpack/CPackDebianConfig.cmake: changed Debian package filename {rsc   -> librsb}....deb; removed conflicts; support CPACK_PACKAGE_REVISION

--- cpack/CPackDebianConfig.cmake
+++ cpack/CPackDebianConfig.cmake
@@ -16,20 +16,20 @@ SET(CPACK_STRIP_FILES TRUE)
 INCLUDE(CheckLSBTypes)
 
 SET(VERSION_SUFFIX "${RSC_VERSION_MAJOR}.${RSC_VERSION_MINOR}")
-SET(CPACK_PACKAGE_VERSION   "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}${CPACK_PACKAGE_REVISION}")
-SET(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}${VERSION_SUFFIX}-${CPACK_PACKAGE_VERSION}__${LSB_PROCESSOR_ARCH}")
 
 SET(CPACK_GENERATOR "DEB")
-SET(CPACK_DEBIAN_PACKAGE_NAME "rsc${VERSION_SUFFIX}")
-SET(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}")
-SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "Sebastian Wrede <swrede@techfak.uni-bielefeld.de>")
+
+SET(CPACK_DEBIAN_PACKAGE_NAME        "lib${RSC_NAME}${VERSION_SUFFIX}")
+SET(CPACK_DEBIAN_PACKAGE_VERSION     "${CPACK_PACKAGE_VERSION}${CPACK_PACKAGE_REVISION}")
+SET(CPACK_DEBIAN_PACKAGE_MAINTAINER  "Sebastian Wrede <swrede@techfak.uni-bielefeld.de>")
 SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Library of reusable C++ and CMake code.
  RSC is a project focused on all aspects of reusable library functions
  in robotics systems. RSC is meant to improve the C++ std lib for
  robotic systems without pulling in many additional dependencies.")
-SET(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
-SET(CPACK_DEBIAN_PACKAGE_SECTION "devel")
-SET(CPACK_DEBIAN_PACKAGE_CONFLICTS "rsc0.6, rsc0.5, rsc0.4")
-SET(CPACK_DEBIAN_ARCHITECTURE ${CMAKE_SYSTEM_PROCESSOR})
-SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-signals-dev, libc6, libboost-program-options-dev, libboost-filesystem-dev, libboost-thread-dev")
-SET(CPACK_DEBIAN_PACKAGE_SUGGESTS "cmake-data, cmake, doxygen, cppcheck, sloccount, lcov")
+SET(CPACK_DEBIAN_PACKAGE_PRIORITY    "optional")
+SET(CPACK_DEBIAN_PACKAGE_SECTION     "devel")
+SET(CPACK_DEBIAN_ARCHITECTURE        ${CMAKE_SYSTEM_PROCESSOR})
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS     "libboost-signals-dev, libc6, libboost-program-options-dev, libboost-filesystem-dev, libboost-thread-dev")
+SET(CPACK_DEBIAN_PACKAGE_SUGGESTS    "cmake-data, cmake, doxygen, cppcheck, sloccount, lcov")
+
+SET(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CMAKE_SYSTEM_PROCESSOR}")

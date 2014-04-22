db10360606eaf431c240cdb104a83ab1b1cbdaec
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-04-22 18:51:54 +0200

    Added POSIX fallbacks for {Host,Process}Info in src/rsc/os
    refs #1966
    
    * src/rsc/os/PosixHostInfo.cpp: new file; minimal POSIX-compliant
      implementation of host information protocol
    * src/rsc/os/PosixProcessInfo.cpp: similar
    * src/CMakeLists.txt: use src/rsc/os/Posix{Host,Process}Info.cpp for
      UNIX systems other than Linux

---
+++ src/rsc/os/PosixHostInfo.cpp
@@ -0,0 +1,70 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2014 Jan Moringen
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include "HostInfo.h"
+
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <stdexcept>
+#include <sstream>
+
+#include <boost/format.hpp>
+
+namespace rsc {
+namespace os {
+
+// Hostname
+
+const unsigned int HOSTNAME_MAX_LENGTH = 1024;
+
+std::string currentHostname() {
+    char buffer[HOSTNAME_MAX_LENGTH];
+    if (gethostname(buffer, HOSTNAME_MAX_LENGTH) == 0) {
+        return std::string(buffer);
+    } else {
+        throw std::runtime_error(boost::str(boost::format("gethostname failed: %1%")
+                                            % strerror(errno)));
+    }
+}
+
+// Host ID
+
+std::string currentHostId() {
+    std::ostringstream stream;
+    stream << std::hex << gethostid(); // cannot fail according to gethostid(3)
+    return stream.str();
+}
+
+// Boot time
+
+boost::posix_time::ptime currentBootTime() {
+    throw std::runtime_error("Could not determine boot time: not supported.");
+}
+
+}
+}
---
+++ src/rsc/os/PosixProcessInfo.cpp
@@ -0,0 +1,75 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2014 Jan Moringen
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include "ProcessInfo.h"
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+namespace rsc {
+namespace os {
+
+unsigned int currentProcessId() {
+    return getpid(); // cannot fail according to getpid(2)
+}
+
+std::string getProgramName(PID /*pid*/) {
+    throw std::runtime_error("Could not determine program name: not"
+                             " supported");
+}
+
+std::string currentProgramName() {
+    return getProgramName(currentProcessId());
+}
+
+std::vector<std::string> getCommandlineArguments(PID /*pid*/) {
+    throw std::runtime_error("Could not determine commandline arguments:"
+                             " not supported");
+}
+
+std::vector<std::string> currentCommandlineArguments() {
+    return getCommandlineArguments(currentProcessId());
+}
+
+std::string getExecutablePath(PID /*pid*/) {
+    throw std::runtime_error("Could not determine executable path:"
+                             " not supported");
+}
+
+std::string currentExecutablePath() {
+    return getExecutablePath(currentProcessId());
+}
+
+boost::posix_time::ptime getProcessStartTime(PID /*pid*/) {
+    throw std::runtime_error("Could not determine process start time:"
+                             " not supported");
+}
+
+boost::posix_time::ptime currentProcessStartTime() {
+    return getProcessStartTime(currentProcessId());
+}
+
+}
+}

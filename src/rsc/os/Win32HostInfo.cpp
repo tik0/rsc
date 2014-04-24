15d115e9e3d7c27eada1ac459693a61c8ae90156
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-04-24 08:37:48 +0200

    Added Win32 implementations of {Host,Process}Info in src/rsc/os
    refs #1966
    
    * src/rsc/os/Win32HostInfo.cpp: new file; implementation of host
      information protocol
    * src/rsc/os/Win32ProcessInfo.cpp: similar
    * src/CMakeLists.txt: use src/rsc/os/Win32{Host,Process}Info.cpp for
      Win32 systems

---
+++ src/rsc/os/Win32HostInfo.cpp
@@ -0,0 +1,79 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2014 Jan Moringen
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include "HostInfo.h"
+
+#define SECURITY_WIN32
+#include <windows.h>
+#include <security.h>
+
+#include <stdexcept>
+#include <sstream>
+
+#include <boost/format.hpp>
+
+namespace rsc {
+namespace os {
+
+// Hostname
+
+const unsigned int HOSTNAME_MAX_LENGTH = 1024;
+
+std::string currentHostname() {
+    char buffer[HOSTNAME_MAX_LENGTH];
+    long unsigned int length = HOSTNAME_MAX_LENGTH;
+    if (GetComputerName(buffer, &length) != 0) {
+        return std::string(buffer, length);
+    } else {
+        throw std::runtime_error(boost::str(boost::format("GetComputerName failed: %1%")
+                                            % GetLastError()));
+    }
+}
+
+// Host ID
+
+const unsigned int HOST_ID_MAX_LENGTH = 1024;
+
+std::string currentHostId() {
+    char buffer[HOST_ID_MAX_LENGTH];
+    long unsigned int length = HOST_ID_MAX_LENGTH;
+    if (GetComputerObjectName(NameUniqueId, buffer, &length) != 0) {
+        return std::string(buffer, length);
+    } else {
+        throw std::runtime_error(boost::str(boost::format("GetComputerObjectName(NameUniqueId)"
+                                                          " failed: %1%")
+                                            % GetLastError()));
+    }
+}
+
+// Boot time
+
+boost::posix_time::ptime currentBootTime() {
+    throw std::runtime_error("Could not get boot time: not supported");
+}
+
+}
+}
---
+++ src/rsc/os/Win32ProcessInfo.cpp
@@ -0,0 +1,119 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2014 Jan Moringen
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include "ProcessInfo.h"
+
+#include <windows.h>
+
+#include <stdexcept>
+
+#include <boost/format.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/filetime_functions.hpp>
+
+namespace rsc {
+namespace os {
+
+PID currentProcessId() {
+    return GetCurrentProcessId(); // Cannot fail according to MSDN
+}
+
+std::string getProgramName(PID /*pid*/) {
+    throw std::runtime_error("Could not determine program name: not supported");
+}
+
+const unsigned int PROGRAM_NAME_MAX_LENGTH = 1024;
+
+std::string currentProgramName() {
+    char buffer[PROGRAM_NAME_MAX_LENGTH];
+    int length;
+    if ((length
+         = GetModuleFileName(NULL, buffer, PROGRAM_NAME_MAX_LENGTH)) == 0) {
+        throw std::runtime_error(boost::str(boost::format("Could not"
+                                                          " determine program"
+                                                          " name: %1%")
+                                            % GetLastError()));
+    }
+    return std::string(buffer, length);
+}
+
+std::string getExecutablePath(PID /*pid*/) {
+    throw std::runtime_error("Could not determine executable path: not supported");
+}
+
+std::string currentExecutablePath() {
+    throw std::runtime_error("Could not determine executable path: not supported");
+}
+
+std::vector<std::string> getCommandlineArguments(PID /*pid*/) {
+    throw std::runtime_error("Could not determine commandline arguments: not supported");
+}
+
+// Based on MSDN example for CommandLineToArgv
+std::vector<std::string> currentCommandlineArguments() {
+    LPWSTR* arguments;
+    int numArguments;
+    if ((arguments = CommandLineToArgvW(GetCommandLineW(), &numArguments))
+         == NULL) {
+        throw std::runtime_error(boost::str(boost::format("Could not determine"
+                                                          " commandline"
+                                                          " arguments: %1%")
+                                            % GetLastError()));
+    }
+
+    std::vector<std::string> result;
+    for (int i = 0; i < numArguments; ++i) {
+        result.push_back(std::string(reinterpret_cast<const char*>(arguments[i])));
+    }
+    LocalFree(arguments);
+
+    return result;
+}
+
+boost::posix_time::ptime getProcessStartTime(PID /*pid*/) {
+    throw std::runtime_error("Could not determine process start time: not supported");
+}
+
+boost::posix_time::ptime currentProcessStartTime() {
+    FILETIME creationTime;
+    FILETIME exitTime;     // not used
+    FILETIME kernelTime;   // not used
+    FILETIME userTime;     // not used
+
+    if (GetProcessTimes(GetCurrentProcess(),
+                        &creationTime, &exitTime, &kernelTime, &userTime)
+        == 0) {
+        throw std::runtime_error(boost::str(boost::format("Could not"
+                                                          " determine process"
+                                                          " start time: %1%")
+                                            % GetLastError()));
+    }
+
+    return boost::date_time::time_from_ftime<boost::posix_time::ptime>(creationTime);
+}
+
+}
+}

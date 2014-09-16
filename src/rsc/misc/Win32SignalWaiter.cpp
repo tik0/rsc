097ccb4a250e0d5e40893050181c44a30ef8e241
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-09-16 18:48:11 +0200

    Win32 implementation of termination signal waiting in src/rsc/misc/Win32SignalWaiter.cpp
    refs #2007
    
    * src/rsc/misc/Win32SignalWaiter.cpp: new file; contains implementation
      stub for Win32 of the termination signal waiting interface
    * src/CMakeLists.txt: added file src/rsc/misc/Win32SignalWaiter.cpp

---
+++ src/rsc/misc/Win32SignalWaiter.cpp
@@ -0,0 +1,69 @@
+/* ============================================================
+ *
+ * This file is part of the RSC project
+ *
+ * Copyright (C) 2014 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include "SignalWaiter.h"
+
+#include <Windows.h>
+
+#include <stdexcept>
+
+#include <iostream>
+
+namespace rsc {
+namespace misc {
+
+void initSignalWaiter(int signals) {
+    // SIGNALS should specify one or more signals.
+    if (((INTERRUPT_REQUESTED | TERMINATE_REQUESTED | QUIT_REQUESTED)
+         & signals) == 0) {
+        throw std::logic_error("At least one signal has to be specified.");
+    }
+}
+
+Signal waitForSignal() {
+    std::cerr << "Waiting for termination signals is not implemented for "
+                 "this platform. The program will still terminate when "
+                 "such a signal is received but without shutting down "
+                 "properly."
+              << std::endl;
+    Sleep(INFINITE);
+    return INTERRUPT_REQUESTED; // should be unreachable
+}
+
+int suggestedExitCode(Signal signal) {
+    switch (signal) {
+    case INTERRUPT_REQUESTED:
+        return 1;
+    case TERMINATE_REQUESTED:
+        return 1;
+    case QUIT_REQUESTED:
+        return 1;
+    }
+    throw std::logic_error("Invalid signal number");
+}
+
+}
+}

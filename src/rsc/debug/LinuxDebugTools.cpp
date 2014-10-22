7d5e3321d5f8590ac99593b3cc5dd9e88fa04b38
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2014-10-22 21:00:12 +0200

    extracting and demangling function names from raw stack trace

--- src/rsc/debug/LinuxDebugTools.cpp
+++ src/rsc/debug/LinuxDebugTools.cpp
@@ -25,8 +25,9 @@
  * ============================================================ */
 
 #include "LinuxDebugTools.h"
-
+#include "rsc/runtime/Demangle.h"
 #include <execinfo.h>
+#include <sstream>
 
 using namespace std;
 
@@ -39,6 +40,32 @@ LinuxDebugTools::LinuxDebugTools() {
 LinuxDebugTools::~LinuxDebugTools() {
 }
 
+// the stacktrace contains the path and a mangled function name
+// <lib name>(<mangled name>+<address offset>) [<address>]
+static string demangle(char* sym) {
+    char *begin = 0, *end = 0;
+    // find the parentheses and address offset surrounding the mangled name
+    for (char *i = sym; *i; ++i) {
+       if (*i == '(') begin = i;
+       else if (*i == '+') end = i;
+    }
+
+    if (begin && end) {
+        // found our mangled name, now in [begin, end)
+        *begin++ = '\0'; // replace '('
+        *end = '\0';     // replace '+'
+
+        ::std::ostringstream oss;
+        try {
+            oss << sym << ": " << rsc::runtime::demangle(begin);
+            return oss.str();
+        } catch (const std::exception &e) {
+        }
+    }
+    // if we come here, demangling failed: just use the whole string
+    return sym;
+}
+
 vector<string> LinuxDebugTools::createBacktrace(const unsigned int& maxElements) {
 
     void** arr = (void**) malloc(maxElements * sizeof(void*));
@@ -47,7 +74,7 @@ vector<string> LinuxDebugTools::createBacktrace(const unsigned int& maxElements)
 
     vector<string> result;
     for (int i = 0; i < nSize; i++) {
-        result.push_back(sym[i]);
+        result.push_back(demangle(sym[i]));
     }
 
     free(sym);
